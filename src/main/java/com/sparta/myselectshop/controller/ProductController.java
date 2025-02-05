12b3aa2d52da3a6e1cbc56c0a656c65256f288cd
Bulgogi-Pizza <quitendexit@gmail.com>
2025-02-05 12:13:50 +0900

    관심상품 희망 최저가 업데이트

---
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -0,0 +1,33 @@
+package com.sparta.myselectshop.controller;
+
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class ProductController {
+
+  private final ProductService productService;
+
+  @PostMapping("/products")
+  public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
+    return productService.createProduct(requestDto);
+  }
+
+  @PutMapping("/products/{id}")
+  public ProductResponseDto updateProduct(@RequestBody ProductMypriceRequestDto requestDto, @PathVariable Long id) {
+    return productService.updateProduct(requestDto, id);
+  }
+
+}
---
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -0,0 +1,40 @@
+package com.sparta.myselectshop.service;
+
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.repository.ProductRepository;
+import jakarta.persistence.EntityExistsException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+  private final ProductRepository productRepository;
+
+  public static final int MIN_MY_PRICE = 100;
+
+  public ProductResponseDto createProduct(ProductRequestDto requestDto) {
+    Product product = productRepository.save(new Product(requestDto));
+
+    return new ProductResponseDto(product);
+  }
+
+  @Transactional
+  public ProductResponseDto updateProduct(ProductMypriceRequestDto requestDto, Long id) {
+    Product product = productRepository.findById(id).orElseThrow(
+        () -> new EntityExistsException("존재하지 않는 마이 상품입니다."));
+
+    if (requestDto.getMyprice() < MIN_MY_PRICE) {
+      throw new IllegalArgumentException("내 가격 설정은 " + MIN_MY_PRICE + "원 이상이어야 합니다.");
+    }
+
+    product.update(requestDto);
+
+    return new ProductResponseDto(product);
+  }
+}
