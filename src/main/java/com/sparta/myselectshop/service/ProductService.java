9c23b3c14c339b18500286d9f520ba89e39c18ca
Bulgogi-Pizza <quitendexit@gmail.com>
2025-02-05 14:54:07 +0900

    상품 페이징 및 정렬

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -5,11 +5,17 @@ import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.entity.User;
+import com.sparta.myselectshop.entity.UserRoleEnum;
 import com.sparta.myselectshop.naver.dto.ItemDto;
 import com.sparta.myselectshop.repository.ProductRepository;
 import jakarta.persistence.EntityExistsException;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -41,10 +47,24 @@ public class ProductService {
     return new ProductResponseDto(product);
   }
 
-  public List<ProductResponseDto> getProducts(User user) {
-    return productRepository.findAllByUser(user).stream()
-        .map(ProductResponseDto::new)
-        .toList();
+  public Page<ProductResponseDto> getProducts(User user, int page, int size, String sortBy,
+      boolean isAsc) {
+
+    Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+    Sort sort = Sort.by(direction, sortBy);
+    Pageable pageable = PageRequest.of(page, size, sort);
+
+    UserRoleEnum userRoleEnum = user.getRole();
+
+    Page<Product> productList;
+
+    if (userRoleEnum == UserRoleEnum.ADMIN) {
+      productList = productRepository.findAll(pageable);
+    } else {
+      productList = productRepository.findAllByUser(user, pageable);
+    }
+
+    return productList.map(ProductResponseDto::new);
   }
 
   @Transactional
@@ -53,11 +73,5 @@ public class ProductService {
         new NullPointerException("해당 상품은 존재하지 않습니다."));
 
     product.updateByItemDto(itemDto);
-  }
-
-  public List<ProductResponseDto> getAllProducts() {
-    return productRepository.findAll().stream()
-        .map(ProductResponseDto::new)
-        .toList();
   }
 }
