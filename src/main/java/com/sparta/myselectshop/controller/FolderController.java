3cb9cf3c16a58f5d2a25a4354c9ac4845431c51d
Bulgogi-Pizza <quitendexit@gmail.com>
2025-02-06 16:25:20 +0900

    @ExceptionHandler 적용

--- src/main/java/com/sparta/myselectshop/controller/FolderController.java
+++ src/main/java/com/sparta/myselectshop/controller/FolderController.java
@@ -2,12 +2,16 @@ package com.sparta.myselectshop.controller;
 
 import com.sparta.myselectshop.dto.FolderRequestDto;
 import com.sparta.myselectshop.dto.FolderResponseDto;
+import com.sparta.myselectshop.exception.RestApiException;
 import com.sparta.myselectshop.security.UserDetailsImpl;
 import com.sparta.myselectshop.service.FolderService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,5 +37,17 @@ public class FolderController {
   @GetMapping("/folders")
   public List<FolderResponseDto> getFolders(@AuthenticationPrincipal UserDetailsImpl userDetails) {
     return folderService.getFolders(userDetails.getUser());
+  }
+
+  @ExceptionHandler({IllegalArgumentException.class})
+  public ResponseEntity<RestApiException> handleException(IllegalArgumentException ex) {
+    System.out.println("FolderController.handleException");
+    RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+    return new ResponseEntity<>(
+        // HTTP body
+        restApiException,
+        // HTTP status code
+        HttpStatus.BAD_REQUEST
+    );
   }
 }
